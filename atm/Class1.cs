54f6ca02d42dbd057a7a812fce8d855816f2a339
ryukomvp <daniel123hernandez15@gmail.com>
2024-05-06 18:30:12 -0600

    Create Class1.cs

---
+++ atm/Class1.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: danie
+ * Date: 06/05/2024
+ * Time: 18:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace atm
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Class1
+	{
+		public Class1()
+		{
+		}
+		ajdklajkldjaslkdjaskljdklasjkl
+	}
+}
